75c5b34d10aa7e71c0792a50f23600fa092a110d
Helge Stenström <h.stenstrom@gmail.com>
2017-09-07 20:24:16 +0200

    Menu är ganska bra.

--- Assignment 2/Assignment2/Menu.cs
+++ Assignment 2/Assignment2/Menu.cs
@@ -2,89 +2,103 @@ using System;
 
 namespace Assignment2
 {
-    class Menu
+  class Menu
+  {
+    private static string aline = "--------------------------------------------------\n";
+    public void Start()
     {
-        private static string aline = "--------------------------------------------------\n";
-        public void Start()
-        {
-            Console.Clear();
-            PrintBanner();
-            PrintChoices();
-            RunChoice();
-            Console.ReadLine();
-        }
-
-        private string Banner ()
-        {
-            //string line   = "--------------------------------------------------\n";
-            string header = "                MAIN MENU                         \n";
-            return aline+header+aline;
-        }
-
-        private void PrintBanner()
-        {
-            Console.Write(Banner());
-        }
-
-        private string MenuItem (string text)
-        {
-            return "   " + text;
-        }
-
-        private void PC (string text)
-        {
-            Console.WriteLine(MenuItem(text));
-        }
-
-
-        private void PrintChoices ()
-        {
-            PC("Whole Numbers with For                : 1");
-            PC("Floating Point Numbers with While     : 2");
-            PC("Currency Converter with Do While loop : 3");
-            PC("Work Schedule                         : 4");
-            PC("Exit the program                      : 0");
-            Console.Write(aline);
-        }
-
-        private string GetChoice()
-        {
-            Console.Write("Your choice: ");
-            return Console.ReadLine();
-        }
-
-        private void RunChoice()
-        {
-            while (true)
-            {
-
-                bool exit = false;
-                switch (GetChoice())
-                {
-                    case "0":
-                        exit = true;
-                        break;
-                    case "1":
-                        Console.WriteLine("Running Whole Numbers");
-                        WholeNumbersForAdd subProgram = new WholeNumbersForAdd();
-                        subProgram.Start();
-                        break;
-                    case "2":
-                        Console.WriteLine("Running Floating Point");
-                        break;
-                    case "3":
-                        Console.WriteLine("Running Currency Converter");
-                        break;
-                    case "4":
-                        Console.WriteLine("Running Work Schedule");
-                        break;
-                    default:
-                        Console.WriteLine("Invalid choice");
-                        break;
-                }
-                if (exit) break;
-            }
-        }
+      Console.Clear();
+      //PrintBanner();
+      Cycle();
+      //PrintChoices();
+      //RunChoice();
+      Console.WriteLine();
+      //Console.ReadLine();
 
     }
+
+    private string Banner ()
+    {
+      //string line   = "--------------------------------------------------\n";
+      string header = "                MAIN MENU                         \n";
+      return aline+header+aline;
+    }
+
+    private void PrintBanner()
+    {
+      Console.Write(Banner());
+    }
+
+    private void Cycle()
+    {
+      while (true)
+      {
+        PrintChoices();
+        string choice = GetChoice();
+        if (choice == "0")
+          break;
+        else
+          RunChoice(choice);
+      }
+
+    }
+
+    private string MenuItem (string text)
+    {
+      return "   " + text;
+    }
+
+    private void PC (string text)
+    {
+      Console.WriteLine(MenuItem(text));
+    }
+
+
+    private void PrintChoices ()
+    {
+      PrintBanner();
+      PC("Whole Numbers with For                : 1");
+      PC("Floating Point Numbers with While     : 2");
+      PC("Currency Converter with Do While loop : 3");
+      PC("Work Schedule                         : 4");
+      PC("Exit the program                      : 0");
+      Console.Write(aline);
+    }
+
+    private string GetChoice()
+    {
+      Console.Write("Your choice: ");
+      return Console.ReadLine();
+    }
+
+    private void RunChoice(string choice)
+    {
+
+      bool exit = false;
+      switch (choice)
+      {
+        case "0":
+          exit = true;
+          break;
+        case "1":
+          Console.WriteLine("Running Whole Numbers");
+            //WholeNumbersForAdd subProgram = new WholeNumbersForAdd();
+            // subProgram.Start();
+          break;
+        case "2":
+          Console.WriteLine("Running Floating Point");
+          break;
+        case "3":
+          Console.WriteLine("Running Currency Converter");
+          break;
+        case "4":
+          Console.WriteLine("Running Work Schedule");
+          break;
+        default:
+          Console.WriteLine("Invalid choice");
+          break;
+      }
+    }
+
+  }
 }
--- Assignment 2/Assignment2/WholeNumbersForAdd.cs
+++ Assignment 2/Assignment2/WholeNumbersForAdd.cs
@@ -6,7 +6,26 @@ namespace Assignment2
   {
     public void Start()
     {
-      Console.WriteLine("WNFA started");
+      Banner();
+      int count = GetValueCount();
+      Console.WriteLine("count = " + count);
     }
+
+    private void Banner()
+    {
+      Console.WriteLine();
+      Console.WriteLine("++++++ Summation of whole numbers ++++++");
+      Console.WriteLine("             Using a for-statement      ");
+      Console.WriteLine();
+    }
+
+    private int GetValueCount()
+    {
+      Console.Write("Number of values to sum? ");
+      int count = int.Parse(Console.ReadLine());
+      Console.WriteLine();
+      return count;
+    }
+
   }
 }
