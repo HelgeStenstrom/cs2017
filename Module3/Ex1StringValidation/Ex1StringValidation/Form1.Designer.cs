153580dd71feaab44d9169da596d867d9e5f7c65
Helge Stenström <helge.stenstrom@ericsson.com>
2017-10-09 16:09:36 +0200

    Ett första steg.

---
+++ Module3/Ex1StringValidation/Ex1StringValidation/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace Ex1StringValidation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.prompt = new System.Windows.Forms.Label();
+            this.NameBox = new System.Windows.Forms.TextBox();
+            this.OKbutton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // prompt
+            // 
+            this.prompt.AutoSize = true;
+            this.prompt.Location = new System.Drawing.Point(51, 52);
+            this.prompt.Name = "prompt";
+            this.prompt.Size = new System.Drawing.Size(118, 13);
+            this.prompt.TabIndex = 0;
+            this.prompt.Text = "Write your name please";
+            this.prompt.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // NameBox
+            // 
+            this.NameBox.Location = new System.Drawing.Point(193, 45);
+            this.NameBox.Name = "NameBox";
+            this.NameBox.Size = new System.Drawing.Size(100, 20);
+            this.NameBox.TabIndex = 1;
+            this.NameBox.TextChanged += new System.EventHandler(this.NameBox_TextChanged);
+            // 
+            // OKbutton
+            // 
+            this.OKbutton.Location = new System.Drawing.Point(123, 113);
+            this.OKbutton.Name = "OKbutton";
+            this.OKbutton.Size = new System.Drawing.Size(75, 23);
+            this.OKbutton.TabIndex = 2;
+            this.OKbutton.Text = "OK";
+            this.OKbutton.UseVisualStyleBackColor = true;
+            this.OKbutton.Click += new System.EventHandler(this.okButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 198);
+            this.Controls.Add(this.OKbutton);
+            this.Controls.Add(this.NameBox);
+            this.Controls.Add(this.prompt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label prompt;
+        private System.Windows.Forms.TextBox NameBox;
+        private System.Windows.Forms.Button OKbutton;
+    }
+}
+
---
+++ Module3/Ex1StringValidation/Ex1StringValidation/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Ex1StringValidation
+{
+    public partial class Form1 : Form
+    {
+        private string name;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine("Användaren klickade OK");
+        }
+
+        private void NameBox_TextChanged(object sender, EventArgs e)
+        {
+            name = e.ToString();
+        }
+    }
+}
